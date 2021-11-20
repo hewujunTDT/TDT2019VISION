6275aaa97c9b7e62703b38f6642907a3bfcbb70a
wujunhe <42902059+hewujunTDT@users.noreply.github.com>
2021-11-20 17:42:25 +0800

    Add files via upload

---
+++ HikVision/demo/64/GrabImage/cmake-build-debug/CMakeFiles/GrabImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GrabImage.dir/GrabImage.cpp.o"
+  "GrabImage.pdb"
+  "GrabImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GrabImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
